cb19c7b849cadde2f4888388e71cae46cf4c70ed
shobinsho <133934377+shobinsho@users.noreply.github.com>
2023-05-18 12:20:57 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "http://girlkiduniya.unaux.com/graphql"
+	const endpoint = "http://bindaybinfor.epizy.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`http://girlkiduniya.unaux.com/` + encodeURI(path as string)
+					`http://bindaybinfor.epizy.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
